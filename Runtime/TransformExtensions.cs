4abe98dcdfc8db25e1c0dc54f45d88c1b7c537fc
Artem <artsel256@gmail.com>
2025-01-19 17:01:16 +0700

    (1.1.0) Added GetZAngleFromDirection()

--- Runtime/TransformExtensions.cs
+++ Runtime/TransformExtensions.cs
@@ -4,10 +4,23 @@ namespace Artmine15.Packages.Utils.Extensions
 {
     public static class TransformExtensions
     {
-        public static void RotateToTargetByZ(this Transform transform, Quaternion fromAngle, Vector2 direction, float maxDegreesDelta = 360, float ZRotationFactor = 0)
+        /// <summary>
+        /// Rotate transform using Mathf.Atan2() and Quaternion.RotateTowards().
+        /// </summary>
+        /// <param name="fromAngle">Start angle that Quaternion.RotateTowards() using.</param>
+        
+        public static void RotateToTargetByZ(this Transform transform, Quaternion fromAngle, Vector2 direction, float maxDegreesDelta = 360, float zRotationFactor = 0)
         {
-            Quaternion angle = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + ZRotationFactor);
+            //Quaternion angle = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + ZRotationFactor);
+            Quaternion angle = transform.GetZAngleFromDirection(direction, zRotationFactor);
             transform.rotation = Quaternion.RotateTowards(fromAngle, angle, maxDegreesDelta);
+        }
+
+        /// <returns>Returns quaternion angle from direction using Mathf.Atan2().</returns>
+        public static Quaternion GetZAngleFromDirection(this Transform transform, Vector2 direction, float zRotationFactor = -90)
+        {
+            Quaternion angle = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + zRotationFactor);
+            return angle;
         }
     }
 }
